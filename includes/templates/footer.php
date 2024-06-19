5cfaa2d0479219f30e2c2c4d5f890647bb866050
JoseArLoLo <josearturolopezlopez2@gmail.com>
2024-06-19 14:28:44 -0700

    Correcciones al sitio web

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,5 +1,24 @@
 <footer>
-    
+    <div class="contenedor">
+        <div class="footer_item">
+            <h1>Contacto</h1>
+            <div class="icons"></div>
+        </div>
+        <div class="footer_item">
+            <div>
+                <p>Benito Juarez S/N Col. Centro</p>
+                <p>C.P. 80000, Culiacan, Sinaloa, Mexico</p>
+            </div>
+            <div>
+                <p>(667) 715 7301</p>
+                <p>(667) 715 7242</p>
+            </div>
+            <div>
+                <p>&#174;2024. Caritas Diocesana de Culiacan Todos los derechos reservados.</p>
+                <p>Aviso de Privacidad.</p>
+            </div>
+        </div>
+    </div>
 </footer>
 <script src="/build/js/app.js"></script>
 <script src="/build/js/modernizr.js"></script>
