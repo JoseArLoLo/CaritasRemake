e969189aecd92211b40113aa68382de8e6f31d01
JoseArLoLo <josearturolopezlopez2@gmail.com>
2024-06-20 14:43:41 -0700

    Correcciones al pie de pagina

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,18 +1,21 @@
 <footer>
-    <div class="contenedor">
+    <div class="contenedor footer_container">
         <div class="footer_item">
             <h1>Contacto</h1>
-            <div class="icons"></div>
-        </div>
-        <div class="footer_item">
             <div>
                 <p>Benito Juarez S/N Col. Centro</p>
                 <p>C.P. 80000, Culiacan, Sinaloa, Mexico</p>
             </div>
+        </div>
+        <div class="footer_item">
+            <div></div>
             <div>
                 <p>(667) 715 7301</p>
                 <p>(667) 715 7242</p>
             </div>
+        </div>
+        <div class="footer_item">
+            <div class="icons"></div>
             <div>
                 <p>&#174;2024. Caritas Diocesana de Culiacan Todos los derechos reservados.</p>
                 <p>Aviso de Privacidad.</p>
