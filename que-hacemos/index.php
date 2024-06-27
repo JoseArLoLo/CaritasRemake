4c405ce3bf5f49560d564b91e454839bd72402f0
JoseArLoLo <josearturolopezlopez2@gmail.com>
2024-06-26 18:52:21 -0700

    Correcciones generales

--- que-hacemos/index.php
+++ que-hacemos/index.php
@@ -10,8 +10,7 @@ incluirTemplate("header", $pagina = "que-hacemos");
             <img loading="lazy" src="/build/img/banner_2.png" alt="Banner que hacemos">
         </picture>
     </div>
-    <h1>EN PROCESO DE CODIFICACION</h1>
-    <section class="contenedor que-hacemos-acciones">
+    <section class="que-hacemos-acciones">
         <div class="menu">
             <div class="menu-item active" data-content="salud">
                 <picture>
@@ -80,36 +79,56 @@ incluirTemplate("header", $pagina = "que-hacemos");
                     </picture>
                 </div>
             </div>
-            <h4>Farmacia</h4>
-            <p>Sabemos que la salud es un derecho básico por lo que nos preocupamos que no haya personas sin posibilidad
-                de adquirir sus medicamentos por lo que en Cáritas tenemos un programa de ayuda que suministra
-                medicamentos a quien lo necesita.</p>
-            <h4>Apoyos especiales</h4>
-            <p>Existen en nuestra sociedad familias y personas que se ven limitadas por situaciones de salud o
-                transportación. En Cáritas hemos destinado un presupuesto especial para estas necesidades. Contamos con
-                un servicio de préstamos de equipos ortopédicos, estudios y atención de especialidades médicas, y el
-                apoyo se enfoca a las personas de bajos recursos económicos que lo soliciten y a aquellos que deseen
-                llegar a su hogar.</p>
+            <div class="salud_item_2">
+                <div class="item">
+                    <h4>Farmacia</h4>
+                    <p>Sabemos que la salud es un derecho básico por lo que nos propusimos que no haya personas sin posibilidad
+                        de adquirir sus medicamentos por lo que en Cáritas hemos equipado una farmacia que suministra medicinas a un costo mínimo.</p>
+                    <br>
+                    <p>
+                        La farmacia está ubicada en las oficinas centrales de Cáritas, además distribuimos medicamento a los dispensarios medicos ubicados en algunas parroquias de la diócesis.
+                    </p>
+                </div>
+                <div class="item">
+                    <h4>Apoyos especiales</h4>
+                    <p>Existen en nuestra sociedad familias y personas que se ven limitadas por situaciones de salud o
+                        transportación. </p>
+                    <br>
+                    <p>En Cáritas hemos destinado un presupuesto mensual para ayudar a aquellos en esta situación. Con un estudio previo y después de seleccionar a los más necesitados, apoyamos de manera profesional a personas que requieren asistencia médica o quirúrgica y a aquellos que desean llegar a su hogar.</p>
+                </div>
+            </div>
         </div>
-
         <div class="content" id="caritas-parroquial">
-            <h2>¿Cuáles son los casos especiales?</h2>
-            <p>El programa de Casos Especiales, ofrece atención personalizada a personas de bajos recursos con
-                necesidades específicas relacionadas con su salud personal, hospitalización de familiares, asesoría y
-                acompañamiento en trámites para el bienestar, necesidad de equipo de rehabilitación y apoyo en consulta
-                médica especializada.</p>
-            <img src="/mnt/data/corte-4.png" alt="Casos especiales">
-            <h3>¿Cuál es el procedimiento que se le da a un Caso Especial?</h3>
-            <ul>
-                <li>Entrevista inicial al solicitante.</li>
-                <li>Aplicación de estudio socioeconómico.</li>
-                <li>Integración de expediente.</li>
-                <li>Solicitud de ayuda para medicamentos.</li>
-                <li>Valoración del médico tratante o del trabajador social.</li>
-                <li>Gestión de apoyo interno económico en especie.</li>
-                <li>Enlace con otras instituciones.</li>
-                <li>Llenado de expediente con apoyo otorgado.</li>
-            </ul>
+            <div class="caritas-parroquial_item_1">
+                <div class="item">
+                    <h4>¿Qué es un Caritas Parroquial?</h4>
+                    <p>Las Cáritas Parroquiales, siendo parte de la Pastoral Social, están formadas por grupos de voluntarios de cada parroquia que, promovidos y animados por el Sacerdote y sensibilizados de la problemática de su comunidad, buscan ayudar a los más desprotegidos a través de programas permanentes que operan en las Parroquias como son: entrega de despensas. comedores comunitarios, consulta médica, dispensario médico, visita a los adultos mayores y enfermos, y, casos especiales.</p>
+                    <p> Todo esto acompañado de una Evangelización integral con educación en valores y la familia. También promovemos la caridad, es decir somos un vínculo entre los que pueden ayudar y los que más lo necesitan, fomentando lo participación de las instituciones de asistencia, así como la vinculación a los diferentes programas de apoyo que existen en gobierno y otros instituciones.</p>
+                </div>
+                <div class="item">
+                    <h4>Objetivo del programa</h4>
+                    <p>Ofrecer una herramienta para el desarrollo del Plan de Pastoral Social en la Parroquias de lo Diócesis de Culiacán de San José. Mediante este modelo y apoyados con la formación y acompañamiento de Cáritas Diocesana buscamos desarrollar acciones organizadas y permanentes para atender a los más desprotegidos. </p>
+                    <picture>
+                        <source srcset="/build/img/frijojes.avif" type="image/avif">
+                        <source srcset="/build/img/frijojes.webp" type="image/webp">
+                        <img loading="lazy" src="/build/img/frijojes.png" alt="imagen de plato de alimento">
+                    </picture>
+                </div>
+            </div>
+            <div class="parroquial_item_2">
+                <div class="item">
+                    <h4>Módulo de atención Caritas Parroquial</h4>
+                    <p>Cáritas Parroquial es un modelo integral que pretende convertir a las comunidades en un motor de caridad cristiana, buscamos que las Córitas parroquiales sean la respuesta a una necesidad inmediata en una determinado situación: los hambrientos han de ser saciados, los desnudos vestidos, los enfermos atendidos para que se recuperen, los ancianos acompañados para que no vivan en soledad, etc. Cada una de las comunidades con conciencia caritativa ha de hacer lo posible para poner a disposición los medios necesarios para atenuar las necesidades existentes en sus propias comunidades.
+                    </p>
+                </div>
+                <div class="item">
+                    <picture>
+                        <source srcset="/build/img/imagen-parroquial.avif" type="image/avif">
+                        <source srcset="/build/img/imagen-parroquial.webp" type="image/webp">
+                        <img loading="lazy" src="/build/img/imagen-parroquial.png" alt="imagen caritas parroquial">
+                    </picture>
+                </div>
+            </div>
         </div>
 
         <div class="content" id="alimentos">
