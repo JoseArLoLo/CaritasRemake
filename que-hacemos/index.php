e9219b1bcc662cd8b854d54a0225d56dc8bf0d48
JoseArLoLo <josearturolopezlopez2@gmail.com>
2024-06-27 22:29:03 -0700

    seccion que hacemos terminada

--- que-hacemos/index.php
+++ que-hacemos/index.php
@@ -130,41 +130,126 @@ incluirTemplate("header", $pagina = "que-hacemos");
                 </div>
             </div>
         </div>
-
         <div class="content" id="alimentos">
-            <h2>¿Qué es un banco de alimentos?</h2>
-            <p>En México se desperdicia un 1/3 de los alimentos que se producen, que equivale a 20 millones de toneladas
-                al año, mientras que por otro lado 1 de cada 4 mexicanos no tiene lo suficiente para comer, esto nos
-                indica que el problema no radica en la escasez de alimentos, sino en su distribución.</p>
-            <img src="/mnt/data/corte-3.png" alt="Banco de alimentos">
-            <h3>Objetivo del programa</h3>
-            <p>Atender la necesidad básica de alimento en personas que viven en situación de vulnerabilidad o en
-                pobreza, redistribuyendo el alimento que se desperdicia en la sociedad, acompañado de acciones
-                orientadas a mejorar el estado nutricional de la población atendida.</p>
-            <p>A través del banco de alimentos, Cáritas otorga alrededor de 3,200 despensas al mes, en beneficio de las
-                personas de menores recursos.</p>
-            <p>Tenemos un comedor en Los Mochis que proporciona despensas y comida caliente a las personas de su área de
-                influencia.</p>
+            <div class="alimentos_item_1">
+                <div class="item">
+                    <h4>¿Qué es un banco de alimentos?</h4>
+                    <p>En México se desperdicia un 1/3 de los alimentos que se producen, que equivale a 20 millones de toneladas
+                        al año, mientras que por otro lado 1 de cada 4 mexicanos no tiene lo suficiente para comer, esto nos
+                        indica que el problema no radica en la existencia de alimentos, si no en su distribución.</p>
+                    <br>
+                    <p>Es aquí cuando intervienen los Bancos de Alimentos, quienes se encargan de captar y rescatar este excedente de alimentos y hacerlos llegar a las personas que más lo necesitan para así combatir el hambre y mejorar la alimentación de miles de familias.</p>
+                </div>
+                <div class="item">
+                    <picture>
+                        <source srcset="/build/img/Imagen-Almentos.avif" type="image/avif">
+                        <source srcset="/build/img/Imagen-Almentos.webp" type="image/webp">
+                        <img loading="lazy" src="/build/img/Imagen-Almentos.png" alt="imagen alimentos">
+                    </picture>
+                </div>
+            </div>
+            <div class="alimentos_item_2">
+                <div class="item">
+                    <h4>Objetivo del programa</h4>
+                    <p>Atender la necesidad básica de alimento en personas que viven en situación de vulnerabilidad a través del rescate y distribución de alimento de manera oportuna y profesional, acompañado de acciones de orientación nutricional, combatiendo el hambre y mejorando asi el estado nutricional de la población atendida.</p>
+                </div>
+                <div class="item">
+                    <br class="jump">
+                    <br class="jump">
+                    <p>A través del banco de alimentos, Cáritas otorga alrededor de 3,200 despensas al mes, en beneficio de más de 10,000
+                        personas de escasos recursos.</p>
+                    <br>
+                    <p class="color-red">Tenemos un comedor en Los Mochis que proporciona despensas y comida caliente a las personas de su área de
+                        influencia.</p>
+                </div>
+            </div>
+            <div class="alimentos_item_3">
+                <div class="item">
+                    <picture>
+                        <source srcset="/build/img/Objetivo_del_programa.avif" type="image/avif">
+                        <source srcset="/build/img/Objetivo_del_programa.webp" type="image/webp">
+                        <img loading="lazy" src="/build/img/Objetivo_del_programa.png" alt="imagen objetivos del programa">
+                    </picture>
+                </div>
+                <div class="item">
+                    <p>Las empresas donan a Cáritas.</p>
+                    <p>Cáritas va por el alimento, lo procesa, selecciona, clasifica y entrega a sus beneficiarios.</p>
+                    <p>Gracias a tu donativo mejora la nutrición de muchos.</p>
+                </div>
+            </div>
+            <div class="alimentos_item_4">
+                <div class="item">
+                    <h4>Nuestros Proyectos</h4>
+                    <p><span class="bold">Despensas:</span> Entrega de paquetes alimenticios cada catorce dias o familias que viven en situación de carencia alimentaria.</p>
+                    <br class="jump">
+                    <p><span class="bold">Nutrición de la primera infancia:</span><br> intervención y monitoreo del estado nutricional de niños menores de 6 años que viven en situación de inseguridad alimentaria para disminuir los índices de mal nutrición y anemia en comunidades agrícolas del valle de Culiacán y Navolato.</p>
+                    <br class="jump">
+                    <p><span class="bold">Comedores comunitarios:</span><br> Entrega de comido preparada por grupos de voluntarios en comedores comunitarios instalados en parroquias a lo largo de la Diócesis de Culiacán.</p>
+                </div>
+                <div class="item">
+                    <br class="jump"><br class="jump">
+                    <p>
+                        <span class="bold">Ubicaciones:</span><br>
+                    <ul>
+                        <li><span>San Bartolomé (Aguaruto. Culiacán)</span></li>
+                        <li><span>Santisimo Sacramento (Culiacán)</span></li>
+                        <li><span>Divina Misericordia (Culiacán)</span></li>
+                        <li><span>Nuestra Señora del Carmen (Culiacán)</span></li>
+                        <li><span>San Francisco de Asís (Navolato)</span></li>
+                    </ul>
+                    </p>
+                    <br class="jump">
+                    <p><span class="bold">Apoyos institucionales:</span><br> Entrega de alimento a instituciones que brindan servicios de alimentación, tales como: casas hogares, asilos para personas de la 3ra edad, comedores comunitarios, centros comunitarios de ayuda, clínicas de rehabilitación, entre otros.</p>
+                </div>
+            </div>
         </div>
 
         <div class="content" id="casos-especiales">
-            <h2>¿Cuáles son los casos especiales?</h2>
-            <p>El programa de Casos Especiales, ofrece atención personalizada a personas de bajos recursos con
-                necesidades específicas relacionadas con su salud personal, hospitalización de familiares, asesoría y
-                acompañamiento en trámites para el bienestar, necesidad de equipo de rehabilitación y apoyo en consulta
-                médica especializada.</p>
-            <img src="/mnt/data/corte-4.png" alt="Casos especiales">
-            <h3>¿Cuál es el procedimiento que se le da a un Caso Especial?</h3>
-            <ul>
-                <li>Entrevista inicial al solicitante.</li>
-                <li>Aplicación de estudio socioeconómico.</li>
-                <li>Integración de expediente.</li>
-                <li>Solicitud de ayuda para medicamentos.</li>
-                <li>Valoración del médico tratante o del trabajador social.</li>
-                <li>Gestión de apoyo interno económico en especie.</li>
-                <li>Enlace con otras instituciones.</li>
-                <li>Llenado de expediente con apoyo otorgado.</li>
-            </ul>
+            <div class="casos-especiales_item_1">
+                <div class="item">
+                    <h4>¿Cuáles son los casos especiales?</h4>
+                    <p>El programa de Casos Especiales, ofrece atención personalizada a personas de bajos recursos con
+                        necesidades específicas relacionadas con su salud personal, hospitalización de familiares, asesoría y
+                        acompañamiento en trámites para el bienestar, necesidad de equipo de rehabilitación y apoyo en consulta
+                        médica especializada.</p>
+                    <br class="jump">
+                    <p class="bold">Este programa es el más abierto de nuestra organización.</p>
+                </div>
+                <div class="item">
+                    <picture>
+                        <source srcset="/build/img/Imagenes_Casos_Especiales.avif" type="image/avif">
+                        <source srcset="/build/img/Imagenes_Casos_Especiales.webp" type="image/webp">
+                        <img loading="lazy" src="/build/img/Imagenes_Casos_Especiales.png" alt="imagen casos especiales">
+                    </picture>
+                </div>
+            </div>
+            <div class="casos-especiales_item_2">
+                <div class="item">
+                    <h4>¿Cuál es el procedimiento que se le da a un Caso Especial?</h4>
+                    <ul>
+                        <li><span>Entrevista inicial al solicitante.</span></li>
+                        <li><span>Aplicación de estudio socioeconómico.</span></li>
+                        <li><span>Integración de expediente.</span></li>
+                        <li><span>Solicitud de ayuda para medicamentos.</span></li>
+                        <li><span>Valoración del médico tratante o del trabajador social.</span></li>
+                        <li><span>Gestión de apoyo interno económico en especie.</span></li>
+                        <li><span>Enlace con otras instituciones.</span></li>
+                        <li><span>Llenado de expediente con apoyo otorgado.</span></li>
+                    </ul>
+                </div>
+                <div class="item">
+                    <h4>¿Qué se le ofrece a un Caso Especial?</h4>
+                    <ul>
+                        <li><span>Consultas de médico general y dental.</span></li>
+                        <li><span>Estudios de laboratorio y gabinete.</span></li>
+                        <li><span>Tratamiento farmacológico a los pacientes que lo requieran con previa evaluación.</span></li>
+                        <li><span>Equipo de ortopedia de rehabilitación (sillas de ruedas, bastones, andaderas, etc.)</span></li>
+                        <li><span>Atención médica domiciliaria a pacientes con enfermedades crónicas degenerativas con discapacidad fisica.</span></li>
+                        <li><span>Acompañamiento espiritual con una celebración eucarística el primer sábado de cada mes.</span></li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     </section>
 </main>
